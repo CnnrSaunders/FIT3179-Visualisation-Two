37687d1b84b07f32e5162d985be8810a9fabf9eb
Connor Saunders <csau0007@student.monash.edu>
2024-09-24 22:03:16 +1000

    added line chart, need to create bar chart, pie chart then structure css

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,9 @@
-var vg_1 = "js/suicide_rate.vg.json";
+var vg_1 = "js/suicide_rate_map.vg.json";
 vegaEmbed("#map_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "js/suicide_rate_line.vg.json";
+vegaEmbed("#line_chart", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
