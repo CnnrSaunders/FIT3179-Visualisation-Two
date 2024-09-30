c8136bc10e4d8d1a8746403d8c6208f0fb42117b
Connor Saunders <csau0007@student.monash.edu>
2024-09-30 20:35:51 +1000

    added new map to index, gonna add appropriate html / css tomos

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -7,3 +7,8 @@ var vg_2 = "js/suicide_rate_line.vg.json";
 vegaEmbed("#line_chart", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "js/suicide_change_time.vg.json";
+vegaEmbed("#map_chart_time", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
