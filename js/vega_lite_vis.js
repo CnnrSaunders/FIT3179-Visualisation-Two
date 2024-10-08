38063398fddcbef5508c65589601f022219a73f2
Connor Saunders <csau0007@student.monash.edu>
2024-10-08 17:04:04 +1100

    fixed width being too big

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,14 +1,19 @@
 var vg_1 = "js/suicide_rate_map.vg.json";
-vegaEmbed("#map_chart", vg_1).then(function(result) {
+vegaEmbed("#map_chart", vg_1, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "js/suicide_rate_line.vg.json";
-vegaEmbed("#line_chart", vg_2).then(function(result) {
+vegaEmbed("#line_chart", vg_2, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "js/suicide_change_time.vg.json";
-vegaEmbed("#map_chart_time", vg_3).then(function(result) {
+vegaEmbed("#map_chart_time", vg_3, {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "js/GdpVsDeathRate_scatter.vg.json";
+vegaEmbed("#scatter_chart", vg_4, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
